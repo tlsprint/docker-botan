2b9bc861991ea928551413aaf96b483bdb6bb41d
Erwin Janssen <erwinjanssen@outlook.com>
2019-09-30 10:56:04 +0200

    Add verification steps for all TLS versions

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+def get_supported_tls(version):
+    """Return a list of all TLS versions supported by the specified version."""
+    # Default to returning the versions supported by tlsprint
+    return ["TLS10", "TLS11", "TLS12"]
+
+
 def extract_versions(tags):
     # Do not include any pre-releases, as those often don't work properly.
     tags = [tag for tag in tags if "rc" not in tag]
@@ -7,5 +13,9 @@ def extract_versions(tags):
         "tag": tag,
         "version": tag,
     } for tag in tags]
+
+    # Add info about supported TLS versions
+    for info in version_info:
+        info["supported_tls"] = get_supported_tls(info["version"])
 
     return version_info
